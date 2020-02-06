48c0d174abc46c1ecac856f12a6f2f4f6a35bcd4
Julian Armour <julian.armour@videotron.ca>
2020-02-05 21:59:47 -0500

    fixed my broken tests

--- src/test/java/app/TestQueryConfig.java
+++ src/test/java/app/TestQueryConfig.java
@@ -2,22 +2,26 @@ package app;
 
 import org.junit.Test;
 
-import java.util.Optional;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class TestQueryConfig {
 
   @Test
   public void parseArgs_NoOptions_DefaultQueryConfig() {
     String[] args = new String[] {"@123.206.85.18", "www.mcgill.ca"};
-    QueryConfig qc = QueryConfig.parseArgs(args);
+    QueryConfig qc = null;
+    try {
+      qc = QueryConfig.parseArgs(args);
+    } catch (BadCommandLineArgument badCommandLineArgument) {
+      fail();
+    }
 
     assertEquals(5, qc.timeout());
     assertEquals(3, qc.maxRetries());
     assertEquals(53, qc.dnsPort());
     assertEquals(QueryType.A, qc.type());
-    assertArrayEquals(new int[]{123, 206, 85, 18}, qc.dnsIP());
+    assertEquals("123.206.85.18", qc.dnsIP());
     assertEquals("www.mcgill.ca", qc.domain());
   }
 
@@ -25,7 +29,12 @@ public class TestQueryConfig {
   @Test
   public void parseArgs_OnlyServerIP_ServerIPSetInConfig() {
     String[] args = new String[] {"@888.888.888.888"};
-    QueryConfiguration qc = QueryConfig.parseArgs(args);
+    QueryConfiguration qc = null;
+    try {
+      qc = QueryConfig.parseArgs(args);
+    } catch (BadCommandLineArgument badCommandLineArgument) {
+      fail();
+    }
     assertEquals("888.888.888.888", qc.dnsIP());
   }
 }
