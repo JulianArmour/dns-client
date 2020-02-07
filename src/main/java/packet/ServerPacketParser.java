8611102a644a5b5b12fead57903aa5cfe3479d34
Julian Armour <julian.armour@videotron.ca>
2020-02-06 21:24:39 -0500

    created ServerPacketParser interface
    Signed-off-by: Julian Armour <julian.armour@videotron.ca>

--- src/main/java/packet/ServerPacketParser.java
+++ src/main/java/packet/ServerPacketParser.java
@@ -1,5 +1,7 @@
 package packet;
 
-public interface ServerPacketParser {
+import java.nio.ByteBuffer;
 
+public interface ServerPacketParser {
+  ServerResponse parseServerResponse(ByteBuffer response);
 }
